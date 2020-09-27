0f51d03a4fd1111e194ea13f53ee8d3ba363ca9b
Aakash Bajaj <aakashbajaj5589@gmail.com>
2020-09-27 21:11:11 +0530

    fix

--- web-server/app.py
+++ web-server/app.py
@@ -15,9 +15,12 @@ pins = {
 
 for k in pins.keys():
     GPIO.setup(pins[k], GPIO.OUT)
-    GPIO.output(pins[k], GPIO.LOW)
-    time.sleep(2)
     GPIO.output(pins[k], GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(pins[k], GPIO.LOW)
+
+
+GPIO.output(pins["MAXFAN"], GPIO.HIGH)
 
 
 @app.route('/', methods=["GET"])
@@ -31,11 +34,12 @@ def home():
 
 @app.route('/smarthome/<device>/<action>', methods=['GET'])
 def switcher(device, action):
-    pass
+    device_key = device.lower()a
+
 
 @app.route('/smarthome/<device>/status', methods=['GET'])
 def status(device):
-    device_key = device.lower()
+    device_key = device.upper()
 
     data = {
         "state": "on" if GPIO.input(pins[device_key]) is 0 else "off"
